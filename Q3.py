4aa054792544f12ccce2ce9d7815715b599a0853
guilhermeeec <guilhermepic2016@gmail.com>
2019-11-20 12:45:13 -0300

    Q3: Indexação das tuplas e identação da main consertadas

--- Q3.py
+++ Q3.py
@@ -1,12 +1,12 @@
 def isCollisionDetected (dict_retang1, dict_retang2):
-    BLx1 = dict_retang1['BottomLeft'](0)
-    BLy1 = dict_retang1['BottomLeft'](1)
-    TRx1 = dict_retang1['TopRight'](0)
-    TRy1 = dict_retang1['TopRight'](1)
-    BLx2 = dict_retang2['BottomLeft'](0)
-    BLy2 = dict_retang2['BottomLeft'](1)
-    TRx2 = dict_retang2['TopRight'](0)
-    TRy2 = dict_retang2['TopRight'](1)
+    BLx1 = dict_retang1['BottomLeft'][0]
+    BLy1 = dict_retang1['BottomLeft'][1]
+    TRx1 = dict_retang1['TopRight'][0]
+    TRy1 = dict_retang1['TopRight'][1]
+    BLx2 = dict_retang2['BottomLeft'][0]
+    BLy2 = dict_retang2['BottomLeft'][1]
+    TRx2 = dict_retang2['TopRight'][0]
+    TRy2 = dict_retang2['TopRight'][1]
     if ((TRx2 >= BLx1) and (TRy2 >= BLy1)) or ((TRx1 >= BLx2) and (TRy1 >= BLy2)):
         return True
     elif ((TRx1 >= BLx2) and (BLy1 <= TRy2)) or ((TRx2 >= BLx1) and (BLy2 <= TRy1)):
@@ -18,7 +18,7 @@ def isCollisionDetected (dict_retang1, dict_retang2):
     else:
         return False
 
-    def main ():
+def main ():
     BLx1 = int(input('BLx1: '))
     BLy1 = int(input('BLy1: '))
     TRx1 = int(input('TRx1: '))
